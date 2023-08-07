1d0bb362820499826d5532ec723890a7a6016ce7
Pedro-Senatus <127903586+Pedro-Senatus@users.noreply.github.com>
2023-08-07 20:39:08 -0300

    Add files via upload

---
+++ Desafio parada - 42 kick/desafio42.py
@@ -0,0 +1,73 @@
+# Resposta 1
+# n1 = int(input("Digite um número: "))
+# n2 = int(input("Digite um número: "))
+
+# print(n1 + n2)
+
+# **************************
+
+# Resposta 2
+# nome = input("Digite o seu nome: ")
+# idade = int(input("Digite sua idade: "))
+# numero = int(input("Digite su número: "))
+# email = input("Digite seu e-mail: ")
+
+# print(nome, idade, numero, email)
+
+# **************************
+
+# Resposta 3
+
+# codigo = int(input("Digite o código do produto (ou 0 para sair): "))
+
+# while codigo != 0:
+#     quantidade = int(input("Digite a quantidade adquirida: "))
+
+#     produto = ""
+#     valor_unitario = 0.0
+
+#     if codigo == 1:
+#         produto = "Caderno"
+#         valor_unitario = 12.00
+#     elif codigo == 2:
+#         produto = "Régua"
+#         valor_unitario = 2.50
+#     elif codigo == 3:
+#         produto = "Borracha"
+#         valor_unitario = 0.25
+#     elif codigo == 4:
+#         produto = "Mochila"
+#         valor_unitario = 50.00
+#     else:
+#         print("Código inválido. Digite um código válido ou 0 para sair.")
+#         codigo = int(input("Digite o código do produto (ou 0 para sair): "))
+#         continue
+
+#     total = valor_unitario * quantidade
+#     print(f"{produto} - R$ {valor_unitario:.2f}")
+#     print("Total a ser pago:", total)
+#     print()
+
+#     codigo = int(input("Digite o código do produto (ou 0 para sair): ")
+# )
+
+# **************************
+
+# Resposta 4
+
+# centimetros = float(input("Digite o valor em centímetros: "))
+
+# metros = centimetros / 100
+
+# print("Os centimentros equivalem a: ", metros)
+
+# **************************
+
+# Resposta 5
+
+# largura = float(input("Digite a largura do quadrado/retângulo: "))
+# altura = float(input("Digite a altura do quadrado/retângulo: "))
+
+# area = largura * altura
+
+# print("A área do quadrado/retângulo é: " , area)
